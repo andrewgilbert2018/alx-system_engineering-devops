1302d40c0e79e7391258b21c4e1d51d79c06b3db
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-05-05 04:29:48 -0700

    100 textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan((/i(?<=from:|to:|flags:).+?(?=\])/)).join(',')
+#a ruby script the a sender phone number,reciever phone and flag
+puts ARGV[0].scan(/i(?<=from:|to:|flags:).+?(?=\])/).join(',')

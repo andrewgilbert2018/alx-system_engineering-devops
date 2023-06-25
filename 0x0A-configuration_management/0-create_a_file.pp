74a43d2ee2e1d9c787f432ebc3775415c9c2c44e
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-25 10:32:38 -0700

    0 creat a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # This manifesto creates a file called school
 file {'/temp/school/':
 ensure  => present,
-path    => '/tmp/holberton',
+path    => '/tmp/school',
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',

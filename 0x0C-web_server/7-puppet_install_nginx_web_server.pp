092ff1eab8e8e0050b3626390a8f94264125c53d
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-06-28 15:25:57 -0700

    7 puppet install nginx web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,6 @@
 # a puppet script that install nginx
-
-package {'nginx':
-ensure => present,
-name   => 'nginx',
-}
-
-service { 'nginx':
-ensure     => running,
-enable     => true,
-hasrestart => true,
-require    => Package['nginx'],
-subscribe  => File_line["add redirect"],
-}
-
-file {'/var/www/html/index.html':
-ensure  =>  present,
-path    => '/var/www/html/index.html',
-content => 'Hello World!\n',
-}
-
-file_line {'add redirect':
-ensure  => present,
-path    => '/etc/nginx/sites-available/default',
-replace => true,
-line    => '	rewrite /redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-match   => '# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000',
+# Install
+exec{ 'Install':
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx && echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html && sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default-backup && redirect="\n\tserver_name bestameh.tech;\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" && sudo sed -i "37i\ $redirect" /etc/nginx/sites-available/default && sudo service nginx start',
+  provider => shell,
 }

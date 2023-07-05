59ac812a42153f703edcce5f5e3731e55c041bb2
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-07-05 03:43:04 -0700

    2 puppet custom http response header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,21 @@
+#a pupppet scripts ensure an nginx installation
+
+exec { '/usr/bin/env apt-get -y update':}
+->package {'nginx':
+ensure => installed,
+}
+
+->file {'/var/www/html/index.html':
+content => 'Holberton School for the win yeah!\n',
+}
+
+->file_line {'add protocol':
+ensure => present,
+path   => '/etc/nginx/sites-available/default',
+after  => 'listen 80 default_server;',
+line   => "\tadd_header X-Served-By ${hostname};",
+}
+
+->service { 'nginx':
+ensure     => running,
+}

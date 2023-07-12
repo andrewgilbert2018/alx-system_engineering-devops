5c26f125ed34009da474cea200cbe04cefd80f97
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-07-12 01:34:55 -0700

    search_algos

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,7 +12,7 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for ALX_SWE Project
  */
 typedef struct listint_s
 {
@@ -30,7 +30,7 @@ typedef struct listint_s
  * @express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
- * for Holberton project
+ * for ALX_SWE Project
  */
 typedef struct skiplist_s
 {

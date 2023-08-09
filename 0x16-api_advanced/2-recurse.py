bd77eb23aa071f82f046c8df4db87f2dadd921b4
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-08-08 20:58:59 -0700

    2 recurse py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,15 +2,27 @@
 """
 A python script that make a recursive call to query the redit API
 """
-import sys
 
-if __name__ == '__main__':
-    recurse = __import__('2-recurse').recurse
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    A function that gets the top ten hot post of a subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    payload = {'after': after}
+    response = requests.get(url,
+                            allow_redirects=False,
+                            params=payload,
+                            headers={'User-Agent': 'Pear'})
+    if response and response.status_code == 200:
+        after = response.json().get('data').get('after')
+        if (after is None):
+            return hot_list
+        post_list = response.json().get('data').get('children')
+        for children in post_list:
+            hot_list.append(children.get('data').get('title'))
+        return recurse(subreddit, hot_list, after)
     else:
-        result = recurse(sys.argv[1])
-        if result is not None:
-            print(len(result))
-        else:
-            print("None")
+        return None

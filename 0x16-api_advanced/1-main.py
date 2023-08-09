4308cdc5d0fa4a1c32905b6d0ffbe97694856242
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-08-08 21:08:45 -0700

    1 main py

---
+++ 0x16-api_advanced/1-main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+1-main
+"""
+import sys
+
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
+wintermancer@lapbox ~/reddit_api/project $ python3 1-main.py programming
+Firebase founder's response to last week's "Firebase Costs increased by 7000%!"
+How a 64k intro is made
+HTTPS on Stack Overflow: The End of a Long Road
+Spend effort on your Git commits
+It's a few years old, but I just discovered this incredibly impressive video of researchers reconstructing sounds from video information alone
+From the D Blog: Introspection, Introspection Everywhere
+Do MVC like it’s 1979
+GitHub is moving to GraphQL for v4 of their API (v3 was a REST API)
+Google Bug Bounty - The 5k Error Page
+PyCon 2017 Talk Videos
+wintermancer@lapbox ~/reddit_api/project $ python3 1-main.py this_is_a_fake_subreddit
+None

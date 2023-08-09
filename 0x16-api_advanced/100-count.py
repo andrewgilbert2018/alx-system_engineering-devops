1b9c4a949cbbfd14026f5ebd0cafb94e254911b1
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-08-08 21:41:35 -0700

    100 count py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,32 +5,53 @@ A python script that request the top ten hot posts
 import requests
 
 
-def count_words(subreddit, word_list, after=None, my_dict={}):
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """Prints counts of given words found in hot posts of a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+        count (int): The parameter of results matched thus far.
     """
-    Gets the top ten hot post of a subreddit
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    payload = {'after': after}
-    response = requests.get(url,
-                            allow_redirects=False,
-                            params=payload,
-                            headers={'User-Agent': 'Pear'})
-    if response and response.status_code == 200:
-        post_list = response.json().get('data').get('children')
-        for children in post_list:
-            title1 = children.get('data').get('title')
-            for word in word_list:
-                try:
-                    my_dict[word] += title1.lower().split().count(word.lower())
-                except KeyError:
-                    my_dict[word] = title1.lower().split().count(word.lower())
-        after = response.json().get('data').get('after')
-        if (after is None):
-            for key, val in sorted(my_dict.items(),
-                                   key=lambda x: (-1*x[1], -1*x[0])):
-                if (val != 0):
-                    print("{}: {}".format(key.lower(), val))
-            return
-        return count_words(subreddit, word_list, after, my_dict)
-    else:
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
         return
+
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
+
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
